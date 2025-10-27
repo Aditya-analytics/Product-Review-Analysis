6c37633060931f80deec946788b33c72603e44fe
Aditya-analytics <adityapatil38945@gmail.com>
2025-10-28 02:32:21 +0530

    update

--- api.py
+++ api.py
@@ -5,7 +5,8 @@ import joblib
 import io
 import os
 
-app = Flask(__name__, static_folder='frontend/dist', static_url_path='')
+app = Flask(__name__, static_folder='frontend/build', static_url_path='')
+
 CORS(app)
 
 # Load the sentiment analysis model
